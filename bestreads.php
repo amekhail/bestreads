6c67fde1c5ba03078c49937e9a146915d200b51c
amekhail <aomekhail@gmail.com>
2020-11-07 13:16:58 -0700

    Initial commit

---
+++ bestreads.php
@@ -0,0 +1,41 @@
+<?php
+// Adam Mekhail
+if(empty($_GET)){
+    // File name: bestreads.php
+    $bookArray = glob ( './books/*' );
+    // print_r($bookArray);
+    echo json_encode ( $bookArray );
+}
+else {
+    $book = $_GET['book'];
+    echoInformation($book);
+}
+
+function echoInformation($book) {
+    $img = "<p><img src='" . $book . "/cover.jpg'" . "alt='not found'></p>";
+    
+    $bookDesc = file($book . "/description.txt");
+    $bookInfo = file($book . "/info.txt");
+    $bookReview = file($book . "/review.txt");
+
+    
+    $info = "<b>  $bookInfo[0] </b><br> $bookInfo[1] <br><br>";
+    $desc =  "$bookDesc[0]<br><br>";
+    $reviewText = "<b>" . $bookReview[0] . " ";
+    for ($i = 0; $i < $bookReview[1]; $i++) {
+        $reviewText .= "*";
+    }
+    $reviewText .= "</b><br>" . $bookReview[2];
+
+    $entireDescrition = $img . $info . $desc . $reviewText;
+
+
+    $oneGiantHTMLstring = "<div class='onereview'> $entireDescrition </div>";
+    echo $oneGiantHTMLstring;
+}
+
+
+
+
+
+?>
